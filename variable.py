56ce63e252448f33070352af0e9fc485828353e4
saniraj1 <maharjankenny5@gmail.com>
2025-11-20 12:01:40 +1100

    Added variable.py

---
+++ variable.py
@@ -0,0 +1,50 @@
+# name="sunny"
+# age=27
+# height=171.5
+# is_student=True
+
+# print(type(name))
+# print(type(age))
+# print(type(height))
+# print(type(is_student))
+
+# # Type casting
+
+# x="5"
+
+# print(int(x)+10)
+
+# naam=10
+
+# print(str(naam)+ " apples")
+
+# # Practise
+
+# x="30"
+# print(type(int(x)))
+# y="4.5"
+# print(type(float(y)))
+# z=100
+# print(type(str(z)))
+
+
+# Input
+# name=input("what is your name")
+# print("hello", name)
+# age=int(input("what is your name"))
+# print("hello", age)
+
+
+# Practise
+
+# name= input("enter ur name")
+# age=int(input("ur age"))
+
+# print(f"hello {name}, your age is {age}" )
+
+no1=int(input("enter first number"))
+no2=int(input("enter first number"))
+
+total=no1 + no2
+
+print(f"the total is {total}")
