9cda0691f869d06696a592ba03725092ec4e6c9c
saniraj1 <maharjankenny5@gmail.com>
2025-11-20 20:55:52 +1100

    Added strfunction.py

---
+++ strfunction.py
@@ -0,0 +1,47 @@
+# name="Aoshieane"
+
+# print(name[5:])
+# print(name.upper())
+# print(name.lower())
+
+
+
+# strip
+
+# text= "   hello       "
+
+# print(text)
+# print(text.strip())
+
+# split() /replace
+
+# sentence="i love python"
+
+# print(sentence.split())
+
+# print(sentence.replace("python","pie"))
+
+# sentence="i love python"
+# print(sentence.replace(" ","_"))
+
+# fstring formatting
+
+# name="ken"
+# age=22
+
+# print(f"my name is {name} and i am {age }years old")
+
+# practise task4
+
+# name=input("enter your name: \n")
+# age=int(input("enter ur age: \n"))
+
+# print(f"your name is {name}. u are {age} yeas old")
+
+# text=input("enter the text")
+
+# print(text.upper())
+
+text = "Elephant"
+
+print(text[-3:])
