d8963a769f51323151eec452c8b677827634401f
saniraj1 <maharjankenny5@gmail.com>
2025-11-21 13:13:15 +1100

    Added operator.py

---
+++ operator.py
@@ -0,0 +1,52 @@
+# comparision operators
+
+# == equal
+# != not equal
+# > greater than
+# < less than
+# >= greater or equal to
+# <= less or equal to 
+
+
+# logical operators
+
+# and both condition must be True 
+# or at least one True 
+# not reverse true/False
+
+
+# practise
+# if else
+# num =int(input("enter num"))
+
+# if num >=1 and num<=10:
+#     print("num lies in between")
+# else:
+#     print("1 or 2")
+
+# nested if 
+# num =10
+
+# if num>0:
+#     print("positive")
+#     if num % 2 == 0:
+#         print("even number")
+
+# practise 1
+# num=int(input("enter the number"))
+
+# if num<0:
+#     print("negative number")
+# elif num==0:
+#     print("zero")
+# else:
+#     print("positive")
+
+# not practise
+
+num=int(input("enter the number"))
+
+if not(num>=1 and num<=10):
+    print("not in between")
+else:
+    print("in between")
